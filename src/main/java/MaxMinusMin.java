3aa0a962641465ae59a4d23857cabde4f39ff4ce
Hoa Do <hoa.do.ma@gmail.com>
2023-08-03 21:46:45 +0000

    Completed Challenge: Max Minus Min

--- src/main/java/MaxMinusMin.java
+++ src/main/java/MaxMinusMin.java
@@ -10,6 +10,28 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+
+        int largestNum = nums.get(0);
+        int smallestNum = nums.get(0);
+        int sizeOfList = nums.size();
+        
+        for (int x = 0; x < sizeOfList; x++) {
+
+            if(largestNum < nums.get(x)) {
+                largestNum = nums.get(x);
+            }
+            
+            if(smallestNum > nums.get(x)) {
+                smallestNum = nums.get(x);
+            }
+        }
+
+        return (largestNum - smallestNum);
     }
 }
+
+
+// use size() method to get size of list interface
+// use size in for loop
+// find largest num and smallest num
+// difference between highest and lowest value
